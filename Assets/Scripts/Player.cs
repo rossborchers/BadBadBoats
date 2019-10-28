a220e02764dd0f8876f1b1505b2ba0794d6aa827
rossborchers <rossborchers@gmail.com>
2019-10-28 20:07:29 +0200

    Converted to 3d XZ plane. Removed some library files that should have been ignored.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RotateControl))]
+public class Player : MonoBehaviour
+{
+    public Material BoatMat;
+    public Material MonsterMat;
+
+    private RotateControl _control;
+    public RotateControl Control
+    {
+        get
+        {
+            if(_control == null)
+            {
+                _control = GetComponent<RotateControl>();
+            }
+
+            return _control;
+        }
+    }
+
+    public KeyCode KeyCode;
+
+    private Point _point;
+
+    public Color defaultColor;
+
+    private void BecomeMonster()
+    {
+        SetMaterials(MonsterMat);
+        if (_control != null) _control.Boost = 100;
+    }
+
+    private void BecomeBoat()
+    {
+        SetMaterials(BoatMat);
+        if (_control != null) _control.Boost = 0;
+    }
+
+    private void SetMaterials(Material material)
+    {
+        //temp hack
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        foreach(Renderer renderer in renderers)
+        {
+            if(!(renderer is TrailRenderer))
+            {
+                renderer.sharedMaterial = material;
+            } 
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Point point = collision.gameObject.GetComponent<Point>();     
+        if (point != null)
+        {
+            _point = point;
+            _point.Hide();
+            GameManager.Instance.PlayerGotPoint(this);
+        }
+        else
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+            if (player != null)
+            {
+                if (player._point != null)
+                {
+                    Respawn();
+                }
+
+                if (_point == null)
+                {
+                    Control.ChangeDirection();
+                }
+                else
+                {
+                    GameManager.Instance.PlayerKilledOther(this);
+                }
+            }
+            else
+            {
+                Control.ChangeDirection();
+            }    
+        }
+    }
+
+    public void ClearPoint()
+    {
+        if(_point != null)
+        {
+            _point.Destroy();
+            _point = null;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if(_point != null)
+        {
+            _point.Destroy();
+        }   
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode))
+        {
+            Control.Rotate = !Control.Rotate;
+        }
+
+        if(_point == null)
+        {
+            BecomeBoat();       
+        }
+        else
+        {
+            BecomeMonster();
+        }
+    }
+
+    public void Respawn()
+    {
+        transform.position = GameManager.Instance.GetRespawnPoint();
+        GameManager.Instance.IncreaseSpeed();
+    }
+}
---
+++ Assets/Scripts/RotateControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateControl : MonoBehaviour
+{
+    public Transform rotate1;
+    public Transform rotate2;
+
+    public float Speed
+    {
+        get;
+        set;
+    }
+
+    public bool Rotate
+    {
+        get;
+        set;
+    }
+
+    public float Boost
+    {
+        get;
+        set;
+    }
+
+    private float direction = 1;
+
+    public void ChangeDirection()
+    {
+        direction *= -1;
+    }
+
+    void Update()
+    {
+        if(Rotate)
+        {
+            transform.RotateAround(rotate1.position, Vector3.up, direction * (Speed + Boost) * Time.deltaTime);
+        }
+        else
+        {
+            transform.RotateAround(rotate2.position, Vector3.up, direction  * -(Speed + Boost) * Time.deltaTime);
+        }
+    }
+}
